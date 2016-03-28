d3c113eda50b4dc8fd9a4b693088dabe6f36a579
calebmer <calebmeredith8@gmail.com>
2016-03-28 18:26:41 -0400

    doc(resource): add todo doc strings

--- src/resource/mod.rs
+++ src/resource/mod.rs
@@ -12,6 +12,7 @@ use urlencoded::QueryMap;
 /// `Request`/`Response` objects, rather it only handles an abstracted
 /// interface. This helps with compliance to the Uniform Interface constraint.
 pub trait Resource {
+  // TODO: doc
   fn route<'a>(&self, _: &'a Definition, _: String) -> Option<Box<Resource + 'a>> {
     None
   }
@@ -19,22 +20,27 @@ pub trait Resource {
   /// Modify self using the query.
   fn query(&mut self, _: &QueryMap) {}
 
+  // TODO: doc
   fn get(&self, _: &Fn(Vec<String>) -> Url, _: &Service) -> Result<Data, Error> {
     Err(Error::new(MethodNotAllowed, "Cannot perform a GET request to this resource."))
   }
 
+  // TODO: doc
   fn post(&self, _: &Fn(Vec<String>) -> Url, _: &Service) -> Result<Data, Error> {
     Err(Error::new(MethodNotAllowed, "Cannot perform a POST request to this resource."))
   }
 
+  // TODO: doc
   fn put(&self, _: &Fn(Vec<String>) -> Url, _: &Service) -> Result<Data, Error> {
     Err(Error::new(MethodNotAllowed, "Cannot perform a PUT request to this resource."))
   }
 
+  // TODO: doc
   fn patch(&self, _: &Fn(Vec<String>) -> Url, _: &Service) -> Result<Data, Error> {
     Err(Error::new(MethodNotAllowed, "Cannot perform a PATCH request to this resource."))
   }
 
+  // TODO: doc
   fn delete(&self, _: &Fn(Vec<String>) -> Url, _: &Service) -> Result<Data, Error> {
     Err(Error::new(MethodNotAllowed, "Cannot perform a DELETE request to this resource."))
   }
